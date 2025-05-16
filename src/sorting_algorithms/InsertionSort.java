cbca440b2289683bf0950c791ccd4ab0e737b004
haseeb0807 <abdulhaseeb.chouhan@zemosolabs.com>
2025-05-16 22:20:23 +0530

    Add insertion sort

--- src/sorting_algorithms/InsertionSort.java
+++ src/sorting_algorithms/InsertionSort.java
@@ -1,4 +1,28 @@
 package sorting_algorithms;
 
+import static sorting_algorithms.Utils.*;
+
 public class InsertionSort {
+
+    static void insertionSort(int[] arr) {
+        for(int i =1; i < arr.length; i++) {
+            int j = i - 1;
+            int key = arr[i];
+
+            while(j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        printArray(arr, "Array - ");
+        insertionSort(arr);
+        printArray(arr, "Sorted Array - ");
+    }
 }
